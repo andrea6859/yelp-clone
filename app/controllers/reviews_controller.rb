3dd48687dd6337b4cf95f88facfc59812efdf870
Andrea Wildsmith <awildsmi@hotmail.com>
2016-10-06 16:57:37 +0100

    extending active record associations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,14 +9,14 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Restaurant.build_with_user(review_params, current_user)
-    # @restaurant.build_review(review_params, current_user)
+    @review = @restaurant.reviews.build_with_user(review_params, current_user)
 
     if @review.save
       redirect_to restaurants_path
     else
-      if @reviews.errors[:user]
-        redirect_to restaurants_path, alert: 'You have already reviewed this restaurant'
+      if @review.errors[:user]
+        flash[:notice] = 'You have already reviewed this restaurant'
+        redirect_to restaurants_path
       else
         render :new
       end
@@ -29,11 +29,5 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:thoughts, :rating)
   end
 
-  def build_review(review_params, current_user)
-    if !current_user.reviewed_restaurants.include? @restaurant
-      @review = reviews.build(review_params)
-      @review.save
-    end
-  end
 
 end
